edfed691f1fb1640c8c7c4959d120a7d16784fcf
peytonfisher <pmnf0713@gmail.com>
2026-05-05 01:09:27 -0400

    merge conflict

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,7 @@ import Hero from '../components/Hero';
 function Home() {
   return (
     <div className="home-container">
-<<<<<<< HEAD
       <Hero />
-=======
->>>>>>> 38e9cd006ff343dbcf9b8caf48c614a934cfe689
     </div>
   );
 }
